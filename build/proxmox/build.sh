0a8e889c4c60ec54d704e6cf28974e515e93ee34
mikejcKS <michael@kryptos-security.com>
2021-10-10 22:52:22 -0400

    Update build.sh

--- build/proxmox/build.sh
+++ build/proxmox/build.sh
@@ -73,10 +73,10 @@ cd pve-kernel
 #  bypasses the process safely.
 # This curl skips certificate validation because Proxmox GIT WebUI doesn't send Let's Encrypt intermediate cert
 echo "Step 2.2: Downloading base kernel"
-curl -f -k "https://git.proxmox.com/?p=mirror_ubuntu-impish-kernel.git;a=snapshot;h=$(git submodule status submodules/ubuntu-impish | cut -c 2-41);sf=tgz" --output kernel.tgz || true
+curl -f -k "https://git.proxmox.com/?p=mirror_ubuntu-impish-kernel.git | cut -c 2-41);sf=tgz" --output kernel.tgz || true
 
 if [[ -f "kernel.tgz" ]]; then
-  tar -xf kernel.tgz -C submodules/ubuntu-impish/ --strip 1
+  tar -xf kernel.tgz -C  1
   rm kernel.tgz
 else
   echo "[-] Failed to download flat base kernel (will use git instead)"
